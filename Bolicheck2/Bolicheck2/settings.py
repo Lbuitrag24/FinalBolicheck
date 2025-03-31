d2dc8443263bcfd0b34dd114f6322d80e7eca416
lbuitrag24 <lebuitrago24@soy.sena.edu.co>
2025-03-31 17:31:39 -0500

    FINAL

--- Bolicheck2/Bolicheck2/settings.py
+++ Bolicheck2/Bolicheck2/settings.py
@@ -64,7 +64,7 @@ SIMPLE_JWT = {
 CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",   
+    'https://finalbolicheckfront.onrender.com',   
 ]
 
 CORS_ALLOW_CREDENTIALS = True
